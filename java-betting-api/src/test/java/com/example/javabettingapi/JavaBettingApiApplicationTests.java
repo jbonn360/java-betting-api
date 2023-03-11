232215cfca1794223b543cd283f4a797ba5128ec
jbonn360 <justinbonnici94@gmail.com>
2023-03-11 10:02:13 +0100

    Initial commit

---
+++ java-betting-api/src/test/java/com/example/javabettingapi/JavaBettingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabettingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBettingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
